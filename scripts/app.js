c7036248a64f0fd7ac7b3b8bd5eea5239f714d78
runrun <runrun0319@163.com>
2018-07-14 11:12:04 +0800

    second commit
    "anthor页面"

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
-//创建app应用模块
-var yike = angular.module("yike",[]);
+//创建app应用模块'
+//依赖控制器模块,在依赖中写上控制器模块的名称Controller
+var yike = angular.module("yike",["Controller","ngRoute"]);
 //run方法在模块创建好之后直接执行
 yike.run(["$rootScope",function($rootScope){
 	// alert(123);
@@ -16,23 +17,42 @@ yike.run(["$rootScope",function($rootScope){
 		//遍历每一个dd,给每一个dd修改样式,显示隐藏/显示的效果
 		//显示时,translate值为0,此时collapsed值为true;
 		if($rootScope.collapsed){
-			for(var i=0; i<dds.length; i++) {
-				dds[i].style.transitionDuration = (i + 1) * 0.15 + 's';
+				for(var i=0; i<dds.length; i++) {
+					dds[i].style.transitionDuration = (i + 1) * 0.15 + 's';
+					dds[i].style.transitionProperty = 'all';
+					dds[i].style.transitionDelay = '0.2s';
+					dds[i].style.transitionTimingFunction = 'ease-out';
+					dds[i].style.transform = 'translate(0)';
+				}
+	        }else{
+	        	for(var i=0; i<dds.length; i++) {
+				dds[i].style.transitionDuration = (dds.length - i + 1) * 0.05 + 's';
 				dds[i].style.transitionProperty = 'all';
-				dds[i].style.transitionDelay = '0.2s';
+				dds[i].style.transitionDelay = '';
 				dds[i].style.transitionTimingFunction = 'ease-out';
-				dds[i].style.transform = 'translate(0)';
-			}
-        }else{
-        	for(var i=0; i<dds.length; i++) {
-			dds[i].style.transitionDuration = (dds.length - i + 1) * 0.05 + 's';
-			dds[i].style.transitionProperty = 'all';
-			dds[i].style.transitionDelay = '';
-			dds[i].style.transitionTimingFunction = 'ease-out';
-			dds[i].style.transform = 'translate(-100%)';
-		    }
-        }
-		
-	}
-	
+				dds[i].style.transform = 'translate(-100%)';
+			    }
+	        }
+			
+		}	
 }]); 
+//修改路由锚点错误bug
+yike.config(["$locationProvider",function($locationProvider){
+	 $locationProvider.hashPrefix("");
+}])
+
+//配置路由
+yike.config(["$routeProvider",function($routeProvider){
+   $routeProvider.when("/",{
+         redirectTo:"/index" //跳转到/index处理
+   }).when("/index",{
+         templateUrl:"./views/list.html",
+         controller:"indexCtrl"
+   }).when("/older",{
+         templateUrl:"./views/older.html",
+         controller:"olderCtrl"
+   }).when("/author",{
+         templateUrl:"./views/author.html",
+         controller:"authorCtrl"
+   })
+}])
